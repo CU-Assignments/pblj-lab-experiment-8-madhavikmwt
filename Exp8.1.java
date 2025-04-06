75d670c2f9b86cefcb3f4ff1947b86c80d31500d
madhavikmwt <22BCS12660@cuchd.in>
2025-04-06 17:16:00 +0530

    Update Exp8.1.java

--- Exp8.1.java
+++ Exp8.1.java
@@ -1,21 +1,50 @@
-Steps to Implement
-1. Set Up Your Environment
-- Install Java Development Kit (JDK)
-- Install Apache Tomcat (Servlet Container)
-- Set up an IDE (Eclipse, IntelliJ, or VScode)
+// Combined Java Servlet Login Example with HTML and web.xml
 
-2. Create an HTML Login Form (login.html)
-This form collects the username and password from the user.
+// login.html
+/*
+<!DOCTYPE html>
+<html>
+<head><title>Login Page</title></head>
+<body>
+<h2>Login</h2>
+<form action="login" method="post">
+    Username: <input type="text" name="username" required><br><br>
+    Password: <input type="password" name="password" required><br><br>
+    <input type="submit" value="Login">
+</form>
+</body>
+</html>
+*/
 
-3. Create the Java Servlet to Process Login (LoginServlet.java)
-This servlet reads username and password from the request.
-It checks the credentials.
-- If valid, it displays a welcome message.
-- If invalid, it redirects back to the login page.
+// LoginServlet.java
+import java.io.*;  
+import javax.servlet.*;  
+import javax.servlet.http.*;  
+public class LoginServlet extends HttpServlet {  
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {  
+        String username = request.getParameter("username");  
+        String password = request.getParameter("password");  
+        response.setContentType("text/html");  
+        PrintWriter out = response.getWriter();  
+        if("admin".equals(username) && "admin123".equals(password)) {  
+            out.println("<h2>Welcome, " + username + "!</h2>");  
+        } else {  
+            out.println("<h2>Invalid credentials. Please try again.</h2>");  
+            out.println("<a href='login.html'>Back to Login</a>");  
+        }  
+    }  
+}
 
-4.  Configure web.xml
-5. Deploy and Run
-- Place the login.html file inside the WebContent (for VScode) or webapp (for Maven projects).
-- Compile and deploy the servlet in Tomcat.
-Access the form in your browser:
-http://localhost:8080/login.html
+// web.xml
+/*
+<web-app xmlns="http://java.sun.com/xml/ns/javaee" version="3.0">
+    <servlet>
+        <servlet-name>LoginServlet</servlet-name>
+        <servlet-class>LoginServlet</servlet-class>
+    </servlet>
+    <servlet-mapping>
+        <servlet-name>LoginServlet</servlet-name>
+        <url-pattern>/login</url-pattern>
+    </servlet-mapping>
+</web-app>
+*/
